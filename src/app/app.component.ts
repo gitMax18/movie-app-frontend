9a6a2dc17839be0e105bc4560fede7342cec6c3a
LAPTOP-ML0416D6\68790 <je.maxime@hotmail.com>
2023-05-02 09:58:47 +1100

    chore : setup angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ``,
+  styles: [``]
 })
 export class AppComponent {
   title = 'movie-app-frontend';
