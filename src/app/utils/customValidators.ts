0b2c9c2f54981caf748ff69f86d4301b668bc6c5
LAPTOP-ML0416D6\68790 <je.maxime@hotmail.com>
2023-05-15 14:05:52 +1100

    feat : add confirmPassword validator

--- src/app/utils/customValidators.ts
+++ src/app/utils/customValidators.ts
@@ -1,13 +1,37 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
 
 export class CustomValidators {
   static invalidNumberValue(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
+    return (control: AbstractControl<number>): ValidationErrors | null => {
       if (control.value < 1901 || control.value > 2155) {
         return {
           invalidNumberValue: `${control.value} is not between 1901 and 2155`,
         };
       }
+      return null;
+    };
+  }
+
+  static confirmPassword(): ValidatorFn {
+    return (
+      control: AbstractControl<{
+        password: FormControl<String>;
+        confirmPassword: FormControl<String>;
+      }>
+    ): ValidationErrors | null => {
+      const password = control.get('password')?.value;
+      const confirmPassword = control.get('confirmPassword')?.value;
+      if (password !== confirmPassword) {
+        return {
+          confirmPassword: 'Password and confirm password must be same',
+        };
+      }
+
       return null;
     };
   }
