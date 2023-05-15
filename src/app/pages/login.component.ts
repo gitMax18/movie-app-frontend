42ccd75def65305f65c097b7679c410078c201b3
LAPTOP-ML0416D6\68790 <je.maxime@hotmail.com>
2023-05-15 14:10:26 +1100

    feat : handle auth form field

--- src/app/pages/login.component.ts
+++ src/app/pages/login.component.ts
@@ -1,25 +1,48 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from '../utils/customValidators';
 
 @Component({
   selector: 'app-login',
   template: `
     <app-form-layout>
       <app-page-title>Login</app-page-title>
-      <form class="form">
+      <form class="form" [formGroup]="loginForm">
         <div class="form__field">
           <label class="form__label" for="email">Email</label>
-          <input class="form__input" type="email" id="email" />
+          <input
+            formControlName="email"
+            class="form__input"
+            type="email"
+            id="email"
+          />
         </div>
         <div class="form__field">
           <label class="form__label" for="password">Password</label>
-          <input class="form__input" type="password" id="password" />
+          <input
+            formControlName="password"
+            class="form__input"
+            type="password"
+            id="password"
+          />
         </div>
         <div class="form__btn">
-          <app-button class="form__btn">Login</app-button>
+          <app-button (onClick)="handleSubmit()" class="form__btn"
+            >Register</app-button
+          >
         </div>
       </form>
     </app-form-layout>
   `,
   styles: [],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl<String>(''),
+    password: new FormControl<String>(''),
+  });
+
+  handleSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
--- src/app/pages/register.component.ts
+++ src/app/pages/register.component.ts
@@ -1,31 +1,92 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from '../utils/customValidators';
 
 @Component({
   selector: 'app-register',
   template: `
     <app-form-layout>
       <app-page-title>Register</app-page-title>
-      <form class="form">
+      <form class="form" [formGroup]="registerForm">
         <div class="form__field">
           <label class="form__label" for="email">Email</label>
-          <input class="form__input" type="email" id="email" />
+          <p
+            class="form__error"
+            *ngIf="registerForm.controls.email.invalid && isInvalidForm"
+          >
+            Must be a valid email
+          </p>
+          <input
+            formControlName="email"
+            class="form__input"
+            type="email"
+            id="email"
+          />
         </div>
         <div class="form__field">
           <label class="form__label" for="password">Password</label>
-          <input class="form__input" type="password" id="password" />
+          <p
+            class="form__error"
+            *ngIf="registerForm.controls.password.invalid && isInvalidForm"
+          >
+            6 characters minimum required
+          </p>
+          <input
+            formControlName="password"
+            class="form__input"
+            type="password"
+            id="password"
+          />
         </div>
         <div class="form__field">
           <label class="form__label" for="confirmPassword"
             >Confirm password</label
           >
-          <input class="form__input" type="password" id="confirmPassword" />
+          <p
+            class="form__error"
+            *ngIf="registerForm.hasError('confirmPassword') && isInvalidForm"
+          >
+            Password and confirm Password must be same
+          </p>
+          <input
+            formControlName="confirmPassword"
+            class="form__input"
+            type="password"
+            id="confirmPassword"
+          />
         </div>
         <div class="form__btn">
-          <app-button class="form__btn">Register</app-button>
+          <app-button (onClick)="handleSubmit()" class="form__btn"
+            >Register</app-button
+          >
         </div>
       </form>
     </app-form-layout>
   `,
   styles: [],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  isInvalidForm = false;
+  registerForm = new FormGroup(
+    {
+      email: new FormControl<String>('', [
+        Validators.email,
+        Validators.required,
+      ]),
+      password: new FormControl<String>('', [
+        Validators.min(6),
+        Validators.required,
+      ]),
+      confirmPassword: new FormControl<String>('', []),
+    },
+    [CustomValidators.confirmPassword()]
+  );
+
+  handleSubmit() {
+    if (this.registerForm.invalid) {
+      this.isInvalidForm = true;
+      return;
+    }
+    console.log(this.registerForm.value);
+  }
+}
